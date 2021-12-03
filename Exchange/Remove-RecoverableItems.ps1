54a18fd21871d58a6c796614a372087d7d90c8f5
Solan <solan@arkuscloud.io>
2021-12-02 20:57:38 -0500

    Create Remove-RecoverableItems.ps1
    Do loop to remove all mail items

---
+++ Exchange/Remove-RecoverableItems.ps1
@@ -0,0 +1,10 @@
+$mbx = get-mailbox "target mailbox"
+
+Do {
+ $result = Search-Mailbox $mbx.Identity -SearchQuery size>0 -SearchDumpsterOnly -DeleteContent -Force -WarningAction Silentlycontinue
+ 
+write-host $result.resultitemscount -ForegroundColor Green
+
+ } Until ($result.resultitemscount -eq 0)
+
+
